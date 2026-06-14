3a8da015c91ab59e8dc02e23c39d38dd3c7a836f
yaragehad <yaragehad06@gmail.com>
2026-06-14 15:15:29 +0200

    added cors to backend to comunicate with frontend, and added venue listing features

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const venueRoutes = require('./routes/venue')
 const bookingRoutes = require('./routes/booking')
 const layoutRoutes = require('./routes/layout')
@@ -7,6 +8,7 @@ const layoutRoutes = require('./routes/layout')
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors())
 app.use(express.json());
 
 const vendorRoutes = require('./routes/vendorRoutes')
@@ -21,6 +23,7 @@ app.use('/api/invoices', invoiceRoutes)
 app.use('/api/venues', venueRoutes)
 app.use('/api/bookings', bookingRoutes)
 app.use('/api/layouts', layoutRoutes)
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Event Management Platform API');

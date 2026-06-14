e09d664f6aa95f1f5e9a7a93b8b05febc0ce0f60
ziad-bakry <159567958+ziad-bakry@users.noreply.github.com>
2026-06-14 15:11:44 +0200

    add organizer booking MVP pages and setup env templates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,9 @@ import VenueCalendarPage from './pages/venue/VenueCalendarPage';
 import BookingRequestsPage from './pages/venue/BookingRequestsPage';
 import LayoutDesignerPage from './pages/venue/LayoutDesignerPage';
 import VenueOwnerDashboard from './pages/venue/VenueOwnerDashboard';
+import OrganizerVenueSearchPage from './pages/organizer/OrganizerVenueSearchPage';
+import OrganizerCreateBookingPage from './pages/organizer/OrganizerCreateBookingPage';
+import OrganizerBookingStatusPage from './pages/organizer/OrganizerBookingStatusPage';
 
 function App() {
   return (
@@ -20,6 +23,9 @@ function App() {
         <Route path="/venue/calendar/:id" element={<VenueCalendarPage />} />
         <Route path="/venue/bookings" element={<BookingRequestsPage />} />
         <Route path="/venue/layout" element={<LayoutDesignerPage />} />
+        <Route path="/organizer/venues" element={<OrganizerVenueSearchPage />} />
+        <Route path="/organizer/bookings/new" element={<OrganizerCreateBookingPage />} />
+        <Route path="/organizer/bookings" element={<OrganizerBookingStatusPage />} />
       </Routes>
     </Router>
   );
